448453a095241d0e9058b6d9f2c0f09b81b52995
sfmajors373 <sfmajors373@gmail.com>
2017-07-09 18:26:06 +0000

    project 3: nearly working not linted

--- src/project-3.js
+++ src/project-3.js
@@ -5,61 +5,102 @@ const makeCat = (name, age) => {
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
   // return the object
+  class Cat {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  meow() {
+      return `Meow!`;
+    }
+  }
+  return new Cat(name, age);
 };
 
 const addProperty = (object, property) => {
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+  object[property] = null;
+  return object;
 };
 
 const invokeMethod = (object, method) => {
   // method is a string that contains the name of a method on the object
   // invoke this method
   // nothing needs to be returned
+  object[method].bind(this);
 };
 
 const multiplyMysteryNumberByFive = (mysteryNumberObject) => {
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
+  return mysteryNumberObject.mysteryNumber * 5;
 };
 
 const deleteProperty = (object, property) => {
   // remove the property from the object
   // return the object
+  delete object[property];
+  return object;
 };
 
 const newUser = (name, email, password) => {
   // create a new object with properties matching the arguments passed in.
   // return the new object
+  class User {
+    constructor(name, email, password) {
+      this.name = name;
+      this.email = email;
+      this.password = password;
+    }
+  };
+  return new User(name, email,password);
 };
 
 const hasEmail = (user) => {
   // return true if the user has a value for the property 'email'
   // otherwise return false
+  if (user.email) {
+    return true;
+  }
+  return false;
 };
 
 const hasProperty = (object, property) => {
   // return true if the object has the value of the property argument
   // property is a string
   // otherwise return false
+  if (object.hasOwnProperty(property)) {
+    return true;
+  }
+  return false;
 };
 
 const verifyPassword = (user, password) => {
   // check to see if the provided password matches the password property on the user object
   // return true if they match
   // otherwise return false
+  let temp = user.password;
+  if (temp === password) {
+    return true;
+  }
+  return false;
 };
 
 const updatePassword = (user, newPassword) => {
   // replace the existing password on the user object with the value of newPassword
   // return the object
+  user.password = newPassword;
+  return user;
 };
 
 const addFriend = (user, newFriend) => {
   // user has a property called friends that is an array
   // add newFriend to the end of the friends array
   // return the user object
+  user.friends.push(newFriend);
+  return user;
 };
 
 const setUsersToPremium = (users) => {
@@ -67,6 +108,10 @@ const setUsersToPremium = (users) => {
   // each user object has the property 'isPremium'
   // set each user's isPremium property to true
   // return the users array
+  for (let i = 0; i < users.length; i++) {
+    users[i].isPremium = true;
+  }
+  return users;
 };
 
 const sumUserPostLikes = (user) => {
@@ -75,6 +120,11 @@ const sumUserPostLikes = (user) => {
   // each post object has an integer property called 'likes'
   // sum together the likes from all the post objects
   // return the sum
+  let temp = 0;
+  for (let i = 0; i < user.posts.length; i++) {
+    temp = temp + user.posts[i].likes;
+  }
+  return temp;
 };
 
 const addCalculateDiscountPriceMethod = (storeItem) => {
@@ -85,6 +135,9 @@ const addCalculateDiscountPriceMethod = (storeItem) => {
   // price -> 20
   // discountPercentage -> .2
   // discountPrice = 20 - (20 * .2)
+  function calculateDiscountPrice() {
+    return (storeItem.price - (storeItem.price * storeItem.discountPercentage));
+  }
 };
 
 // Do not modify code below this line.
